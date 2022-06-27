4d7812d6810474a7595d7d20d333447783c3b273
aesbe <107854618+aesbe@users.noreply.github.com>
2022-06-27 11:40:50 +0700

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "support@nasional.net.id"; // Change this email to your //
+$to = "support@nasional.net.id"; // support@nasional.net.id //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
